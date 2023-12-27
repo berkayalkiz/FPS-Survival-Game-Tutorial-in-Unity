c6c14849d3c79cfc5c18cc0b9c884515845c0d2d
berkayalkiz <alkizberkay0@gmail.com>
2023-12-27 23:38:19 +0300

    add zoom in and out animations scripts

--- FPSSurvivalGame/Assets/Scrips/Player Scripts/PlayerAttack.cs
+++ FPSSurvivalGame/Assets/Scrips/Player Scripts/PlayerAttack.cs
@@ -11,9 +11,24 @@ public class PlayerAttack : MonoBehaviour
     private float nextTimeToFire;
     public float damage = 20f;
 
+    private Animator zoomCameraAnim;
+    private bool zoomed;
+
+    private Camera mainCam;
+
+    private GameObject crosshair;
+
+    private bool is_Aiming;
+
     private void Awake()
     {
         weapon_Manager = GetComponent<WeaponManager>();
+
+        zoomCameraAnim = transform.Find(Tags.LOOK_ROOT).transform.Find(Tags.ZOOM_CAMERA).GetComponent<Animator>();
+
+        crosshair = GameObject.FindWithTag(Tags.CROSSHAIR);
+
+        mainCam = Camera.main;
     }
 
 
@@ -26,6 +41,7 @@ public class PlayerAttack : MonoBehaviour
     void Update()
     {
         WeaponShoot();
+        ZoomInAndOut();
     }
 
     void WeaponShoot()
@@ -52,18 +68,71 @@ public class PlayerAttack : MonoBehaviour
                     weapon_Manager.GetCurrentSelectedWeapon().ShootAnimation();
                 }
 
+                // bullettype is bullet weapons
                 if (weapon_Manager.GetCurrentSelectedWeapon().bulletType == WeaponBulletType.BULLET)
                 {
                     weapon_Manager.GetCurrentSelectedWeapon().ShootAnimation();
 
 
                 }
+
+                // arrow and spear
                 else
                 {
+                    if (is_Aiming)
+                    {
+                        weapon_Manager.GetCurrentSelectedWeapon().ShootAnimation();
 
+                        if(weapon_Manager.GetCurrentSelectedWeapon().bulletType == WeaponBulletType.ARROW)
+                        {
+
+                        }
+
+                        else if (weapon_Manager.GetCurrentSelectedWeapon().bulletType == WeaponBulletType.SPEAR)
+                        {
+
+                        }
+                    } 
                 }
-
             }
         }
-    }
+    } // weapon shoot func
+
+    void ZoomInAndOut()
+    {
+        if (weapon_Manager.GetCurrentSelectedWeapon().weapon_Aim == WeaponAim.AIM)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                zoomCameraAnim.Play(AnimationTags.ZOOM_IN_ANIM);
+
+                crosshair.SetActive(false);
+            }
+
+            if (Input.GetMouseButtonUp(1))
+            {
+                zoomCameraAnim.Play(AnimationTags.ZOOM_OUT_ANIM);
+
+                crosshair.SetActive(true);
+            }
+        }
+
+        if(weapon_Manager.GetCurrentSelectedWeapon().weapon_Aim  == WeaponAim.SELF_AIM)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                weapon_Manager.GetCurrentSelectedWeapon().Aim(true);
+
+                is_Aiming = true;
+            }
+
+            if (Input.GetMouseButtonUp(1))
+            {
+                weapon_Manager.GetCurrentSelectedWeapon().Aim(false);
+
+                is_Aiming = false;
+            }
+
+        }
+    }// zoom in and out func
 }
