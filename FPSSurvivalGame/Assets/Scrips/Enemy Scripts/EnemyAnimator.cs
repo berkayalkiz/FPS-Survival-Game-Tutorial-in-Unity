678a8dd55020c649f0f5d98d3f990c89864f1177
berkayalkiz <alkizberkay0@gmail.com>
2023-12-29 03:23:41 +0300

    add enemy controller script (end of the day)
    4:58:06

--- FPSSurvivalGame/Assets/Scrips/Enemy Scripts/EnemyAnimator.cs
+++ FPSSurvivalGame/Assets/Scrips/Enemy Scripts/EnemyAnimator.cs
@@ -12,13 +12,24 @@ public class EnemyAnimator : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    void Update()
-    {
-        
-    }
 
     public void Walk(bool walk)
     {
         anim.SetBool(AnimationTags.WALK_PARAMETER, walk);
+    }
+
+    public void Run(bool run)
+    {
+        anim.SetBool(AnimationTags.RUN_PARAMETER, run);
+    }
+
+    public void Attack()
+    {
+        anim.SetTrigger(AnimationTags.ATTACK_TRIGGER);
+    }
+
+    public void Dead()
+    {
+        anim.SetTrigger(AnimationTags.DEAD_TRIGGER);
     }
 }
